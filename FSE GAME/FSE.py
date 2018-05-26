f72f8799b9eeb629fed5e1511da3bcf31575628e
Chobaka78 <34846941+Chobaka78@users.noreply.github.com>
2018-05-26 00:51:24 -0400

    Update 10
    Major update
    - re- organized the entire code
    - removed unessesary functions

--- FSE GAME/FSE.py
+++ FSE GAME/FSE.py
@@ -28,7 +28,6 @@ VY=2
 ONGROUND=3
 
 firstBack = image.load("images/firstBack.png")
-LEVEL1back=image.load("images/Level1Back.png")
 cont_button = image.load("images/continue-button.png")
 backgroundRect=Rect(0,0,1080,720)
 buttonRect = Rect(940,10,130,50)
@@ -72,109 +71,16 @@ def menu():
             else:
                 draw.rect(screen,YELLOW,buttons[i],2)            
         display.flip()
-        
-def moveBATMAN(BATMAN):
-    keys = key.get_pressed()
-    global move, frame, rapid, bullets, bullet
-    newMove = -1        
-    if keys[K_LEFT] and BATMAN[X] > 10:
-        newMove = LEFT
-        BATMAN[X] -= 10
-        if BATMAN[SCREENX] > 10:
-            BATMAN[SCREENX] -= 10
-
-    if keys[K_RIGHT] and BATMAN[X] < 2090:
-        newMove = RIGHT
-        BATMAN[X] += 10
-        if BATMAN[SCREENX] < 1000:
-            BATMAN[SCREENX] += 10
-
-    if keys[K_UP] and BATMAN[ONGROUND]:
-        newMove = Jump
-        BATMAN[VY] = -10
-        BATMAN[ONGROUND]=False
-
-    if keys[K_SPACE]:
-        if rapid < 10:
-            rapid+=1
-        if keys[K_SPACE] and rapid==10:
-            rapid = 0
-            VX = 5
-            VY1 = 0
-            bullets.append([BATMAN[X],BATMAN[Y]+20,VX,VY1])
-
-    BATMAN[Y]+=BATMAN[VY]     # add current speed to Y
-    if BATMAN[Y] >= 650:
-       BATMAN[Y] = 650
-       BATMAN[VY] = 0
-       BATMAN[ONGROUND]=True
-    BATMAN[VY]+=.7     # add current speed to Y
-
-    if keys[K_b]:
-        newMove = Punch
-
-    elif newMove == -1:
-        frame = 0
-
-    if move == newMove:     # 0 is a standing pose, so we want to skip over it when we are moving
-        frame = frame + 0.4 # adding 0.2 allows us to slow down the animation
-        if frame >= len(pics[move]):
-            frame = 1
-    elif newMove != -1:     # a move was selected
-        move = newMove      # make that our current move
-        frame = 1
-
-def enemyMove(ALIEN):
-    keys = key.get_pressed()
-    global move2, frame2, HEALTH, heal, bullets
-    newMove2 = -1
-    print("ENmOVE",BATMAN[X],ALIEN[X])
-    if BATMAN[X] < ALIEN[X] and ALIEN[X] > 10:
-        newMove2 = LEFT
-        ALIEN[X] -= 5
-        if ALIEN[SCREENX] > 10:
-            ALIEN[SCREENX] -= 10
-
-    if BATMAN[X] > ALIEN[X] and ALIEN[X] < 2090:
-        newMove2 = RIGHT
-        ALIEN[X] += 5
-        if ALIEN[SCREENX] < 1000:
-            ALIEN[SCREENX] += 10
-
-    if BATMAN[X] == ALIEN[X] and BATMAN[Y] == ALIEN[Y]:
-        newMove2 = -1
-        frame2 = 0
-        ALIEN[X] +=0
-        if HEALTH > 0:
-            HEALTH -=5
-            heal = int(heal * (HEALTH/100))
-
-    if move2 == newMove2:     # 0 is a standing pose, so we want to skip over it when we are moving
-        frame2 = frame2 + 0.4 # adding 0.2 allows us to slow down the animation
-        if frame2 >= len(Epics[move2]):
-            frame2 = 1
-    elif newMove2 != -1:     # a move was selected
-        move2 = newMove2      # make that our current move
-        frame2 = 1
-    return BATMAN
                 
 def makeMove(name,start,end):
-    ''' This returns a list of pictures. They must be in the folder "name"
-        and start with the name "name".
-        start, end - The range of picture numbers 
-    '''
     move = []
     for i in range(start,end+1):
         move.append(image.load("%s/%s%03d.png" % (name,name,i)))        
     return move
 
 def moveEnemy(name,start,end):
-    ''' This returns a list of pictures. They must be in the folder "name"
-        and start with the name "name".
-        start, end - The range of picture numbers 
-    '''
     move2 = []
-    
+
     for i in range(start,end+1):
         move2.append(image.load("%s/%s%03d.png" % (name,name,i)))
         
@@ -195,58 +101,144 @@ def enemyHealth():
     draw.rect(screen,RED,(ALIEN[X],ALIEN[Y]-20,35,5),0)
     draw.rect(screen,GREEN,(ALIEN[X],ALIEN[Y]-20,35,5),0)
 
-def drawScene(screen,BATMAN):
-    bullet = image.load("batman/bullet.png")
-    """ draws the current state of the game """
-    offset = BATMAN[SCREENX] - BATMAN[X]
-    screen.blit(LEVEL1back,(offset,0))
-
-    for b in bullets[:]:
-        b[0]+=b[2]
-        b[1]+=b[3]
-
-        if max(b) > 1080 or min(b) < -0:
-            bullets.remove(b)
-
-    for b in bullets:
-        screen.blit(bullet,(int(b[0]),int(b[1])))
-
-    pic = pics[move][int(frame)]
-    screen.blit(pic, (BATMAN[SCREENX],BATMAN[Y]))
-
-    pic2 = Epics[move2][int(frame2)]
-    screen.blit(pic2, (ALIEN[X],ALIEN[Y]))
-
-    health()
-    enemyHealth()
-    display.flip()
-
-'''
-    The guy's x position is where he is in the "world" we then draw the map
-    at a negative position to compensate.
-'''
-
-def Level1():
+def Game():
+    global BATMAN, heal, HEALTH, ALIEN, move, frame, rapid, bullets, bullet, move2, frame2, HEALTH, heal, bullets
+    level = "1"
+    HEALTH = 100
+    heal = 290
     BATMAN = [10,650,0,True,10]  # Batmans position in the game
+    ALIEN = [1080,650,0,True,10]  # Aleins position in the game
     mixer.music.stop()
     mixer.music.load("Music/Game music 2.mp3")
     mixer.music.play(-1)
     running = True
     myClock = time.Clock()
+    ## Loading all images
+    bullet = image.load("batman/bullet.png")
+    ###########################################
+
+    if level == "1":
+        LEVEL1back=image.load("images/Level1Back.png")
+
     while running:
+
+        offset = BATMAN[SCREENX] - BATMAN[X]
+        screen.blit(LEVEL1back,(offset,0))
+
         for evnt in event.get():          
             if evnt.type == QUIT:
                 running = False
+                
+        keys = key.get_pressed()
+        newMove = -1   
+        ############ MOVING BATMAN ###############     
+        if keys[K_LEFT] and BATMAN[X] > 10:
+            newMove = LEFT
+            BATMAN[X] -= 10
+            if BATMAN[SCREENX] > 10:
+                BATMAN[SCREENX] -= 10
+
+        if keys[K_RIGHT] and BATMAN[X] < 2090:
+            newMove = RIGHT
+            BATMAN[X] += 10
+            if BATMAN[SCREENX] < 1000:
+                BATMAN[SCREENX] += 10
+
+        if keys[K_UP] and BATMAN[ONGROUND]:
+            newMove = Jump
+            BATMAN[VY] = -10
+            BATMAN[ONGROUND]=False
+
+        if keys[K_SPACE]:
+            if rapid < 10:
+                rapid+=1
+            if keys[K_SPACE] and rapid==10:
+                rapid = 0
+                VX = 5
+                VY1 = 0
+                bullets.append([BATMAN[X],BATMAN[Y]+20,VX,VY1])
+
+        BATMAN[Y]+=BATMAN[VY]     # add current speed to Y
+        if BATMAN[Y] >= 650:
+           BATMAN[Y] = 650
+           BATMAN[VY] = 0
+           BATMAN[ONGROUND]=True
+        BATMAN[VY]+=.7     # add current speed to Y
+        ################################################################
+
+        ######## BATMAN PUNCH #######
+        if keys[K_b]:
+            newMove = Punch
+
+        elif newMove == -1:
+            frame = 0
+        ##############################
+
+        ########## ANIMATION FOR BATMAN ############
+        if move == newMove:     # 0 is a standing pose, so we want to skip over it when we are moving
+            frame = frame + 0.4 # adding 0.2 allows us to slow down the animation
+            if frame >= len(pics[move]):
+                frame = 1
+        elif newMove != -1:     # a move was selected
+            move = newMove      # make that our current move
+            frame = 1
+        ############################################
 
         mx, my = mouse.get_pos()
-        print(mx,my)
+        print(mx,my) 
 
-        keys = key.get_pressed()
+        ############ MOVING THE ENEMY ###################
+        newMove2 = -1
+        print("ENmOVE",BATMAN[X],ALIEN[X])
+        if BATMAN[X] < ALIEN[X] and ALIEN[X] > 10:
+            newMove2 = LEFT
+            ALIEN[X] -= 5
+            if ALIEN[SCREENX] > 10:
+                ALIEN[SCREENX] -= 10
 
-        moveBATMAN(BATMAN)       
-        #checkCollide(BATMAN,plats)
-        BATMAN=enemyMove(ALIEN) 
-        drawScene(screen,BATMAN)
+        if BATMAN[X] > ALIEN[X] and ALIEN[X] < 2090:
+            newMove2 = RIGHT
+            ALIEN[X] += 5
+            if ALIEN[SCREENX] < 1000:
+                ALIEN[SCREENX] += 10
+
+        if BATMAN[X] == ALIEN[X] and BATMAN[Y] == ALIEN[Y]:
+            newMove2 = -1
+            frame2 = 0
+            ALIEN[X] +=0
+            if HEALTH > 0:
+                HEALTH -=5
+                heal = int(heal * (HEALTH/100))
+
+        if move2 == newMove2:     # 0 is a standing pose, so we want to skip over it when we are moving
+            frame2 = frame2 + 0.4 # adding 0.2 allows us to slow down the animation
+            if frame2 >= len(Epics[move2]):
+                frame2 = 1
+        elif newMove2 != -1:     # a move was selected
+            move2 = newMove2      # make that our current move
+            frame2 = 1     
+        ##########################################################
+        
+        ############# MOVING THE BULLETS #############
+        for b in bullets[:]:
+            b[0]+=b[2]
+            b[1]+=b[3]
+
+            if max(b) > 1080 or min(b) < -0:
+                bullets.remove(b)
+
+        for b in bullets:
+            screen.blit(bullet,(int(b[0]),int(b[1])))
+        ##############################################
+
+        pic = pics[move][int(frame)]
+        screen.blit(pic, (BATMAN[SCREENX],BATMAN[Y]))
+
+        pic2 = Epics[move2][int(frame2)]
+        screen.blit(pic2, (ALIEN[X],ALIEN[Y]))
+
+        health()
+        enemyHealth()
         display.update()
         myClock.tick(25)
         display.flip()
@@ -366,10 +358,6 @@ Y=1
 VY=2
 ONGROUND=3
 SCREENX = 4
-HEALTH = 100
-heal = 290
-BATMAN = [10,650,0,True,10]  # Batmans position in the game
-ALIEN = [250,650,0,True,10]  # Batmans position in the game
 running = True
 x,y = 0,0
 OUTLINE = (150,50,30)
@@ -378,7 +366,7 @@ while page != "exit":
     if page == "menu":
         page = menu()
     if page == "game":
-        page = Level1()    
+        page = Game()    
     if page == "instructions":
         page = instructions()    
     if page == "story":
@@ -387,6 +375,3 @@ while page != "exit":
         page = credit()    
     
 quit()
-
-# This is the change
-# My name is Usman and I like big cocks
